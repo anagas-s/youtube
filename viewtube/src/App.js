fbb6bbfc5e0d8a4e48abfe41f37aeb447f03aca7
Anagas <115894733+anagas-s@users.noreply.github.com>
2024-03-13 23:13:47 +0530

    Initialise starter files and components

---
+++ viewtube/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Head from './components/Head';
+import Body from './components/Body';
+
+function App() {
+  return (
+    <div>
+      <h1>Namaste React</h1>
+      <Head/>
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
