149a1a2db474f4e776c56e0b25d3f73680ed73b4
Anagas <115894733+anagas-s@users.noreply.github.com>
2024-03-16 10:00:37 +0530

    Implement the header section of the youtube

--- viewtube/src/App.js
+++ viewtube/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Head from './components/Head';
 import Body from './components/Body';
@@ -6,7 +6,7 @@ import Body from './components/Body';
 function App() {
   return (
     <div>
-      <h1>Namaste React</h1>
+     
       <Head/>
       <Body/>
     </div>
--- viewtube/src/components/Head.js
+++ viewtube/src/components/Head.js
@@ -2,7 +2,20 @@ import React from "react";
 
 const Head = () => {
     return(
-        <div>Head</div>
+    <div className="grid grid-flow-col p-4 m-2 shadow-lg">
+        <div className="flex col-span-1">
+            <img className="h-12 -mt-2" alt="menu" src="https://www.svgrepo.com/show/312300/hamburger-menu.svg"></img>
+            <img className="h-8 mx-2" alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"></img>
+        </div>
+        <div className="col-span-10 px-12">
+            <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text"></input>
+            <button className="border border-gray-400 p-2 rounded-r-full px-5 bg-gray-100">🔍</button>    
+        </div>
+        <div className="col-span-1">
+            <img className="h-8" alt="user" src="https://static.vecteezy.com/system/resources/previews/019/879/198/non_2x/user-icon-on-transparent-background-free-png.png"></img>
+        </div>
+
+    </div>
     )
 
 }
