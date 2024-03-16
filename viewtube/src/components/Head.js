5f9d1cb6ed414dfceb79c70aff14c2723276133d
Anagas <115894733+anagas-s@users.noreply.github.com>
2024-03-16 11:16:53 +0530

    Implement sidebar functionality using redux store

--- viewtube/src/components/Head.js
+++ viewtube/src/components/Head.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
 
 const Head = () => {
+
+    const dispatch = useDispatch();
+    const toggleMenuHandler = () => {
+        dispatch(toggleMenu())
+    }
+
     return(
     <div className="grid grid-flow-col p-4 m-2 shadow-lg">
         <div className="flex col-span-1">
-            <img className="h-12 -mt-2" alt="menu" src="https://www.svgrepo.com/show/312300/hamburger-menu.svg"></img>
+            <img onClick={() => toggleMenuHandler()} className="h-12 -mt-2 cursor-pointer" alt="menu" src="https://www.svgrepo.com/show/312300/hamburger-menu.svg"></img>
             <img className="h-8 mx-2" alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"></img>
         </div>
         <div className="col-span-10 px-12">
