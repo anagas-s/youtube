8a78c6cab95c2e4d5209bbb7286aac11584af6af
Anagas <115894733+anagas-s@users.noreply.github.com>
2024-03-29 16:39:47 +0530

    Add n-level nesting for comment section

--- viewtube/src/components/CommentsContainer.js
+++ viewtube/src/components/CommentsContainer.js
@@ -43,7 +43,7 @@ const commentsData =  [
 const Comment = ({data}) => {
     const {name,text,replies} = data
     return( 
-        <div className="flex shadow-sm bg-gray-100 p-2 rounded-lg">
+        <div className="flex shadow-sm bg-gray-100 p-2 my-2 rounded-lg">
         <img className="w-10" alt="user" src="https://static.vecteezy.com/system/resources/previews/019/879/198/non_2x/user-icon-on-transparent-background-free-png.png"/>
         <div className="px-3">
             <p className="font-bold">{name}</p>
@@ -56,12 +56,26 @@ const Comment = ({data}) => {
     )
 }
 
+const CommentsList = ({comments}) => {
+    return <div>
+        {comments.map(comment => {
+            return (
+            <div>
+            <Comment  key={Math.random(Math.random())} data={comment} />
+            <div className="pl-5 border border-l-black ml-5">
+                <CommentsList comments = {comment.replies} />
+            </div>
+            </div>
+            )
+        })}
+    </div>
+}
 
 const CommentsContainer = () => {
     return (
         <div className="m-5 p-2">
             <h1 className="text-2xl font-bold">Comments</h1>
-            <Comment data={commentsData[0]}/>
+            <CommentsList comments={commentsData}/>
         </div>
     )
 }
